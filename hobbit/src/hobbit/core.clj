fa04a223e75bf884b923eda3d6d9a9f991246b2e
Roy Chen <todundsteuern@fattyf.at>
2022-12-01 11:33:54 +0800

    Define function to hit those nasty hobbitses

--- hobbit/src/hobbit/core.clj
+++ hobbit/src/hobbit/core.clj
@@ -50,6 +50,19 @@
           []
           asym-body-parts))
 
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
 (comment
   (symmetrize-body-parts asym-hobbit-body-parts)
-  (better-symmetrize-body-parts asym-hobbit-body-parts))
+  (better-symmetrize-body-parts asym-hobbit-body-parts)
+
+  (hit asym-hobbit-body-parts))
