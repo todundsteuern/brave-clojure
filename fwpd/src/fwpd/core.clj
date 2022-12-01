ed18525b71228243567925b40da0dec3ae57bfd2
Roy Chen <todundsteuern@fattyf.at>
2022-12-01 11:34:19 +0800

    Work on FWPD vampire analysis program

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -1,7 +1,59 @@
 (ns fwpd.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def filename "suspects.csv")
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(str/split % #",")
+       (str/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter)
+          records))
+
+(comment
+  (->> filename
+       slurp
+       parse
+       first
+       (map vector vamp-keys)
+       (reduce (fn [result [vamp-key value]]
+                 (assoc result vamp-key (convert vamp-key value)))
+               {}))
+
+  (-> filename
+      slurp
+      parse
+      mapify)
+
+  (->> filename
+       slurp
+       parse
+       mapify
+       (glitter-filter 3)))
